d40bad8cd0b23b14a9e8fed35c4484ece282fc93
[기획] 이두형 <dazelius86@gmail.com>
2026-01-12 11:47:13 +0900

    Lower camera and look up to show walls

--- ddoo-game/engine/ddoo-background.js
+++ ddoo-game/engine/ddoo-background.js
@@ -25,11 +25,11 @@ const DDOOBackground = {
     // Camera defaults - fit grid exactly, hide back edge
     cameraDefaults: {
         posX: 5,       // Center of arena (X axis)
-        posY: 2.8,     // ★ 높게 (벽면 더 보임)
+        posY: 1.2,     // ★ 낮게 (위쪽 벽면 더 보임)
         posZ: 3.0,     // ★ 확대
         lookAtX: 5,    // Look at arena center
-        lookAtY: -0.2, // ★ 아래 보기
-        lookAtZ: 1.0   // ★ 중앙
+        lookAtY: 0.8,  // ★ 위쪽 보기 (벽면 더 보임)
+        lookAtZ: 0.5   // ★ 가까이
     },
     
     // Auto zoom settings
